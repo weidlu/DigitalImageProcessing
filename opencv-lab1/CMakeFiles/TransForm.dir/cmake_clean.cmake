836b399dc4444ed775e29501cf8dcfd0468875ba
WaldenLu <972672562@qq.com>
2017-03-12 20:24:32 +0800

    编译后生成文件

---
+++ opencv-lab1/CMakeFiles/TransForm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TransForm.dir/Transform.cpp.o"
+  "TransForm.pdb"
+  "TransForm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TransForm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
