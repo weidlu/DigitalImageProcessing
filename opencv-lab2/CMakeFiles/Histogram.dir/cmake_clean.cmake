0445a407ec346624aff663fc5a9f749743745a8e
WaldenLu <972672562@qq.com>
2017-03-19 22:26:15 +0800

    makefile

---
+++ opencv-lab2/CMakeFiles/Histogram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Histogram.dir/histogram.cpp.o"
+  "Histogram.pdb"
+  "Histogram"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Histogram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
