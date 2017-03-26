d696af6bfb8cc4ff5a4acebde7174b2bee47badf
WaldenLu <972672562@qq.com>
2017-03-26 10:45:34 +0800

    lab3

---
+++ opencv-lab3/CMakeFiles/Filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Filter.dir/filter.cpp.o"
+  "Filter.pdb"
+  "Filter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
