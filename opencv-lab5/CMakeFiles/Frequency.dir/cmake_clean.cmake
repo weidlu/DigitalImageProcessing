519cb088f7e17b5d66c85f7908d297023a77ed89
WaldenLu <972672562@qq.com>
2017-04-03 17:25:39 +0800

    else

---
+++ opencv-lab5/CMakeFiles/Frequency.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Frequency.dir/fdma.cpp.o"
+  "Frequency.pdb"
+  "Frequency"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Frequency.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
